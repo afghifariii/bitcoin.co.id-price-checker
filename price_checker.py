b377edb674f18c43bb5813d72eb4f940d4eb7efa
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2018-02-12 19:25:03 +0700

    add jump_percent function

--- price_checker.py
+++ price_checker.py
@@ -8,21 +8,23 @@ def price_checker(target, per):
     resp = requests.get(url).json()
     return float(resp['ticker']['last'])
 
+def jump_percent(current, last):
+    delta = abs(current - last)
+    return (delta / last) * 100
+
 def run_checker(target, per):
     global last_price, current_price
     current_price = price_checker(target, per)
     if (current_price > last_price):
         if last_price > 0.0:
-            increase = current_price - last_price
-            percentage = (increase / last_price) * 100
-            if (percentage > 5.0):
+            percent = jump_percent(current_price, last_price)
+            if (percent > 5.0):
                 winsound.PlaySound('resources/up.wav', winsound.SND_FILENAME)
         print('%.8f' % current_price)
     elif (current_price < last_price):
-        decrease = last_price - current_price
-        percentage = (decrease / last_price) * 100
+        percent = jump_percent(current_price, last_price)
         print('%.8f' % current_price)
-        if (percentage > 5.0):
+        if (percent > 5.0):
             winsound.PlaySound('resources/down.wav', winsound.SND_FILENAME)
 
 while True:
